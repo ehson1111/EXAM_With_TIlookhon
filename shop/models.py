be6243a4ab09864551fa3f44f876fd601370dbdf
Ehson <jamshedehsoni@gmil.com>
2025-05-25 16:34:19 +0500

    add a new models

--- shop/models.py
+++ shop/models.py
@@ -33,4 +33,14 @@ class Order(models.Model):
     
     
 
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in Order {self.order.id}"
+    
+    
+
     
